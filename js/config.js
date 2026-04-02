5f9769b0ce301b18370c85d6070161cfd51867de
leovha <eurodieselvilhena2020@hotmail.com>
2026-04-02 16:06:20 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 const _SB_URL = "https://nqpdhwdkpqjcmvxgaypm.supabase.co";
 const _SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xcGRod2RrcHFqY212eGdheXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzc4MTQsImV4cCI6MjA5MDcxMzgxNH0.oV-G2iGKOEzPUw0jGK9BL14_kx30UqjJ0wXu8xloZdw";
 
-// Criando a conexão com nome exclusivo
+// Mudamos o nome para supabaseClient para o navegador não travar
 const supabaseClient = supabase.createClient(_SB_URL, _SB_KEY);
 
 function verificarSessao() {
