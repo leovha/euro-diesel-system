5eb09fb24c176738c37dce2ef2c5dbf4f8f1853c
leovha <eurodieselvilhena2020@hotmail.com>
2026-04-02 15:50:15 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
+// js/config.js
+const _SB_URL = "https://nqpdhwdkpqjcmvxgaypm.supabase.co";
+const _SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xcGRod2RrcHFqY212eGdheXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzc4MTQsImV4cCI6MjA5MDcxMzgxNH0.oV-G2iGKOEzPUw0jGK9BL14_kx30UqjJ0wXu8xloZdw";
 
+const supabase = supabase.createClient(_SB_URL, _SB_KEY);
+
+// Esta função será usada em todas as páginas para garantir que
+// só quem logou possa ver o conteúdo.
+function verificarSessao() {
+    const usuarioLogado = localStorage.getItem('usuario_logado');
+    if (!usuarioLogado && !window.location.href.includes('index.html')) {
+        window.location.href = '../index.html';
+    }
+}
