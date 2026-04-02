965ec2c79eacbe1ee774d58b0ca51a69ce23ec76
leovha <eurodieselvilhena2020@hotmail.com>
2026-04-02 16:09:57 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 const _SB_URL = "https://nqpdhwdkpqjcmvxgaypm.supabase.co";
 const _SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xcGRod2RrcHFqY212eGdheXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMzc4MTQsImV4cCI6MjA5MDcxMzgxNH0.oV-G2iGKOEzPUw0jGK9BL14_kx30UqjJ0wXu8xloZdw";
 
-// Mudamos o nome para supabaseClient para o navegador não travar
-const supabaseClient = supabase.createClient(_SB_URL, _SB_KEY);
+// Mudamos o nome para 'db' para ser curto e não dar conflito
+const db = supabase.createClient(_SB_URL, _SB_KEY);
 
 function verificarSessao() {
     const usuarioLogado = localStorage.getItem('usuario_logado');
