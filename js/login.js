f3052b3be4b9badabb0f2d1a70fd11a2a342b20e
leovha <eurodieselvilhena2020@hotmail.com>
2026-04-02 16:00:39 -0300

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,23 @@
+// js/login.js
+async function entrar() {
+    const user = document.getElementById('user').value.toUpperCase();
+    const pass = document.getElementById('pass').value;
+
+    // Busca na tabela MAIÚSCULA que já tem a ZANE cadastrada
+    const { data, error } = await supabaseClient
+        .from('USUARIOS') 
+        .select('*')
+        .eq('nome', user)
+        .eq('senha', pass)
+        .single();
+
+    if (error || !data) {
+        alert("Acesso Negado: Usuário ou Senha incorretos!");
+    } else {
+        localStorage.setItem('usuario_logado', data.nome);
+        localStorage.setItem('nivel_acesso', data.nivel);
+        
+        alert("Bem-vindo(a), " + data.nome);
+        window.location.href = "pages/dashboard.html"; 
+    }
+}
